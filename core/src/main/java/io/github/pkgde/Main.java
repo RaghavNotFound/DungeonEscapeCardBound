4f5aa7ad0570d4b2b1f421eeab3dd5729518877e
Raghav Singhal <raghavsinghal638@gmail.com>
2026-03-27 18:41:09 +0530

    Fucking MinorChanges

--- core/src/main/java/io/github/pkgde/Main.java
+++ core/src/main/java/io/github/pkgde/Main.java
@@ -1,12 +1,7 @@
 package io.github.pkgde;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
     @Override
     public void create() {
